b2943a46607de7a1f56b963b561b4468ab60c3f0
BISEL Jean <jean@langmatt.com>
2022-03-31 17:40:28 +0200

    ?

--- src/graphics/shapes/Shape.java
+++ src/graphics/shapes/Shape.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import graphics.shapes.attributes.Attributes;
-
+// find the bug
 public abstract class Shape {
 	
 	private Map<String,Attributes> attributes;
