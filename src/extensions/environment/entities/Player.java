47451eca7dff608df68a50c9f29f79356be8a4f7
Simon Kempf <simon25042001@gmail.com>
2022-04-02 22:50:23 +0200

    gravité composantes

--- src/extensions/environment/entities/Player.java
+++ src/extensions/environment/entities/Player.java
@@ -19,16 +19,38 @@ public class Player extends Entity {
 	@Override
 	public void applyPhysics(TileMap tileMap) {
 		velocity.setLocation(velocity.x, velocity.y+GRAVITY);
-		if(!onGround(tileMap))
-			this.translate(velocity.x, velocity.y);
-		else
-			velocity.setLocation(0, 0);
+		for(float vx = 0; vx < velocity.x; vx = vx+1) //vitesse en x positive
+		{
+			if(!onGround(tileMap, this.getLoc().x+vx+1, 0))
+				this.translate(1, 0);
+			else
+				velocity.setLocation(0, velocity.y);
+		}
+		for(float vx = 0; vx > velocity.x; vx = vx-1) //vitesse en x négative
+		{
+			if(!onGround(tileMap, this.getLoc().x-vx-1, 0))
+				this.translate(-1, 0);
+			else
+				velocity.setLocation(0, velocity.y);
+		}
+		for(float vy = 0; vy < velocity.y; vy = vy+1) //vitesse en yx positive
+		{
+			if(!onGround(tileMap, 0, this.getLoc().y+vy+1))
+				this.translate(0, 1);
+			else
+				velocity.setLocation(velocity.x, 0);
+		}
+		for(float vy = 0; vy > velocity.y; vy = vy-1) //vitesse en y négative
+		{
+			if(!onGround(tileMap, 0, this.getLoc().y-vy-1))
+				this.translate(0, -1);
+			else
+				velocity.setLocation(velocity.x, 0);
+		}
 	}
 
-	public Boolean onGround(TileMap tileMap)
+	public Boolean onGround(TileMap tileMap, float x, float y)
 	{
-		float x = this.getLoc().x+velocity.x;
-		float y = this.getLoc().y+velocity.y;
 		return tileMap.getTextureMap()[(int) (y/tileMap.TILEWIDTH)+1][(int) (x/tileMap.TILEWIDTH)+1] >= 0;
 	}
 }
