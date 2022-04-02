071a4ed34e048a5be1a4202fd2b80f47ac839d84
Simon Kempf <simon25042001@gmail.com>
2022-04-03 00:31:19 +0200

    bug

--- src/extensions/environment/entities/Player.java
+++ src/extensions/environment/entities/Player.java
@@ -18,31 +18,33 @@ public class Player extends Entity {
 
 	@Override
 	public void applyPhysics(TileMap tileMap) {
+		int x = this.getLoc().x;
+		int y = this.getLoc().y;
 		velocity.setLocation(velocity.x, velocity.y+GRAVITY);
 		for(float vx = 0; vx < velocity.x; vx = vx+1) //vitesse en x positive
 		{
-			if(!onGround(tileMap, this.getLoc().x+vx+1, 0))
+			if(!onGround(tileMap, x+vx+tileMap.TILEWIDTH, y))
 				this.translate(1, 0);
 			else
 				velocity.setLocation(0, velocity.y);
 		}
 		for(float vx = 0; vx > velocity.x; vx = vx-1) //vitesse en x négative
 		{
-			if(!onGround(tileMap, this.getLoc().x-vx-1, 0))
+			if(!onGround(tileMap, x+vx-tileMap.TILEWIDTH, y))
 				this.translate(-1, 0);
 			else
 				velocity.setLocation(0, velocity.y);
 		}
-		for(float vy = 0; vy < velocity.y; vy = vy+1) //vitesse en yx positive
+		for(float vy = 0; vy < velocity.y; vy = vy+1) //vitesse en y positive
 		{
-			if(!onGround(tileMap, 0, this.getLoc().y+vy+1))
+			if(!onGround(tileMap, x, y+vy+tileMap.TILEWIDTH))
 				this.translate(0, 1);
 			else
 				velocity.setLocation(velocity.x, 0);
 		}
 		for(float vy = 0; vy > velocity.y; vy = vy-1) //vitesse en y négative
 		{
-			if(!onGround(tileMap, 0, this.getLoc().y-vy-1))
+			if(!onGround(tileMap, x, y+vy-tileMap.TILEWIDTH))
 				this.translate(0, -1);
 			else
 				velocity.setLocation(velocity.x, 0);
@@ -51,6 +53,6 @@ public class Player extends Entity {
 
 	public Boolean onGround(TileMap tileMap, float x, float y)
 	{
-		return tileMap.getTextureMap()[(int) (y/tileMap.TILEWIDTH)+1][(int) (x/tileMap.TILEWIDTH)+1] >= 0;
+		return tileMap.getTextureMap()[(int) (y/tileMap.TILEWIDTH)][(int) (x/tileMap.TILEWIDTH)] >= 0;
 	}
 }
