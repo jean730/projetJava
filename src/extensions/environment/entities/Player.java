ab40bfb6b5863f5724b79877d7b016c78ece6a0a
Simon Kempf <simon25042001@gmail.com>
2022-04-04 20:32:32 +0200

    plafond

--- src/extensions/environment/entities/Player.java
+++ src/extensions/environment/entities/Player.java
@@ -7,9 +7,7 @@ import extensions.environment.TileMap;
 
 public class Player extends Entity {
 	
-	private final double GRAVITY = 0.01; 
-	 
-	private Boolean onRoof;
+	private final double GRAVITY = 0.01;
 	private Point2D.Double velocity = new Point2D.Double(0,0);
 	
 	public Player(Point2D.Double loc) {
@@ -33,6 +31,18 @@ public class Player extends Entity {
 					this.DoubleTranslate(0.0, 1.0);
 			}
 		}
+		else if (velocity.y < 0) {
+			if (!onRoof(tileMap,nextLoc)) {
+				this.setDoubleLoc(nextLoc);
+			}
+			else {
+				this.velocity.y = 0;
+				this.setDoubleLoc(new Point2D.Double(doubleLoc.x,Math.ceil(doubleLoc.y)));
+				doubleLoc = this.getDoubleLoc();
+				while (!onRoof(tileMap,doubleLoc))
+					this.DoubleTranslate(0.0, -1.0);
+			}
+		}
 	}
 	
 	public Point2D.Double nextLoc(double dt) {
@@ -44,6 +54,16 @@ public class Player extends Entity {
 	{
 		try {
 			return tileMap.getTextureMap()[(int) ((doubleLoc.y+this.getSprite().getHeight())/tileMap.TILEWIDTH)][(int) (doubleLoc.x/tileMap.TILEWIDTH)] >= 0;
+		} catch (Exception e) {
+			this.die();
+			return true;
+		}
+	}
+	
+	public Boolean onRoof(TileMap tileMap, Point2D.Double doubleLoc)
+	{
+		try {
+			return tileMap.getTextureMap()[(int) ((doubleLoc.y+this.getSprite().getHeight())/tileMap.TILEWIDTH)-1][(int) (doubleLoc.x/tileMap.TILEWIDTH)] >= 0;
 		} catch (Exception e) {
 			this.die();
 			return true;
