978423958cce8c72ec96d28da14e2dc2bac57360
BISEL Jean <jean@langmatt.com>
2022-05-01 02:44:28 +0200

    changements nuages

--- src/extensions/environment/ui/Environment.java
+++ src/extensions/environment/ui/Environment.java
@@ -46,12 +46,12 @@ public class Environment extends JFrame {
                 StaticEntity fire = new StaticEntity(new Point2D.Double(100,192),"assets/Details/fire.png"); // Entité de test
                 fire.getSprite().registerAnimation("default",new Animation(32,48,256,48,0,8,80));
                 fire.getSprite().setAnimation("default");
-                this.model.addEntity(new Cloud(new Point2D.Double(-200,50),-20,"assets/GrassLand/Background/GrassLand_Cloud_1.png"));
-                this.model.addEntity(new Cloud(new Point2D.Double(-100,80),-30,"assets/GrassLand/Background/GrassLand_Cloud_2.png"));
-                this.model.addEntity(new Cloud(new Point2D.Double(0,20),-20,"assets/GrassLand/Background/GrassLand_Cloud_3.png"));
-                this.model.addEntity(new Cloud(new Point2D.Double(100,100),-40,"assets/GrassLand/Background/GrassLand_Cloud_1.png"));
-                this.model.addEntity(new Cloud(new Point2D.Double(250,30),-10,"assets/GrassLand/Background/GrassLand_Cloud_2.png"));
-                this.model.addEntity(new Cloud(new Point2D.Double(400,60),-50,"assets/GrassLand/Background/GrassLand_Cloud_3.png"));
+                this.model.addEntity(new Cloud(model,new Point2D.Double(-200,50),-20,"assets/GrassLand/Background/GrassLand_Cloud_1.png"));
+                this.model.addEntity(new Cloud(model,new Point2D.Double(-100,80),-30,"assets/GrassLand/Background/GrassLand_Cloud_2.png"));
+                this.model.addEntity(new Cloud(model,new Point2D.Double(0,20),-20,"assets/GrassLand/Background/GrassLand_Cloud_3.png"));
+                this.model.addEntity(new Cloud(model,new Point2D.Double(100,100),-40,"assets/GrassLand/Background/GrassLand_Cloud_1.png"));
+                this.model.addEntity(new Cloud(model,new Point2D.Double(250,30),-10,"assets/GrassLand/Background/GrassLand_Cloud_2.png"));
+                this.model.addEntity(new Cloud(model,new Point2D.Double(400,60),-50,"assets/GrassLand/Background/GrassLand_Cloud_3.png"));
 		this.model.addEntity(p);
 		this.model.addEntity(fire);
 		this.model.addPlayer(p);
